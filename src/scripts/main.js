e4c551994ac60d5029cb3135835c406cf4a9d0d5
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-02-25 15:39:42 +0200

    make build

--- src/scripts/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-document.querySelector('.mobmenu__bur').addEventListener('click', function() {
-  document.querySelector('.mobmenu__bur').classList.toggle('active');
-  document.querySelector('.mobmenu__menu').classList.toggle('active');
-});
